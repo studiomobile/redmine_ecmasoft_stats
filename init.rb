a622b7d8d5db36655aa4b3b3a3535e5e3b9c0d36
Maxim Dobryakov <max.dobryakov@ecmasoft.com>
2010-08-09 19:10:40 +0400

    Changed information about plugin

--- init.rb
+++ init.rb
@@ -1,10 +1,10 @@
 require 'redmine'
 
 Redmine::Plugin.register :redmine_ecmasoft_stats do
-  name 'Redmine Ecmasoft Stats plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  name 'Ecmasoft Statistics plugin'
+  author 'Verbin Andrey/Martynov Sergey/Maxim Dobriakov'
+  description 'Plagin for calculate statistic about development'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/martynovs/redmine_ecmasoft_stats'
+  author_url 'https://github.com/martynovs/redmine_ecmasoft_stats'
 end
